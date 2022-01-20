4359e9866ae5eb21d813150000345b50cf1b1134
Maxwell Wang <90732327+mwang840@users.noreply.github.com>
2022-01-19 21:37:09 -0500

    Update botmain.js

--- botmain.js
+++ botmain.js
@@ -4,11 +4,22 @@ const Discord = require('discord.js');
 //Just created a discord bot
 const client = new Discord.Client();
 
+const prefix = '+';
 
 client.once('ready', ()=>{
     console.log('Mr Diktovitch is online and looking for rent!');
 });
 
+client.on('message', message=>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
 
+    const args = message.content.slice(prefix.length).split(" ");
+    const command = args.shift().toLowerCase();
+
+
+    if(command === 'ping'){
+        message.channel.send('You have received a pong!');
+    }
+});
 
 client.login(tokens.bot);
