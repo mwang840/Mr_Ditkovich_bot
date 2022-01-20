26e203ff85f815a009f69782cdbe6ad587bc4a62
Cameron Thacker <cthacker@udel.edu>
2022-01-19 23:11:42 -0500

    Added somewhat of a folder structure to project

--- src/botmain.js
+++ src/botmain.js
@@ -1,5 +1,5 @@
-const tokens = require('tokens.js');
-
+const tokens = require('./tokens.js');
+var fs = require('fs');
 const Discord = require('discord.js');
 //Just created a discord bot
 const client = new Discord.Client({
@@ -8,7 +8,11 @@ const client = new Discord.Client({
         'status': 'online',
         'afk': false,
         'activities': [{ 'name': 'Waiting for rent', 'type': 'PLAYING', 'url': 'https://www.youtube.com/channel/UCHCqPwHUde2P-idHPUTMoqA' }]
-    }
+    },
+    'intents': [
+        'GUILDS', 'GUILD_BANS', 'GUILD_MEMBERS', 'GUILD_EMOJIS_AND_STICKERS', 'GUILD_INTEGRATIONS', 'GUILD_WEBHOOKS', 'GUILD_INVITES', 'GUILD_VOICE_STATES', 'GUILD_MESSAGES',
+        'GUILD_MESSAGE_REACTIONS', 'GUILD_MESSAGE_TYPING', 'DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS', 'DIRECT_MESSAGE_TYPING', 'GUILD_SCHEDULED_EVENTS'
+    ]
 
 });
 
