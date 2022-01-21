b47e5052ffed8a7be5fa70e583969a2e171bb3cc
Cameron Thacker <cthacker@udel.edu>
2022-01-20 23:09:35 -0500

    Added error logger as well

--- src/server/log_admin_activity.ts
+++ src/server/log_admin_activity.ts
@@ -1,8 +1,10 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-export const WriteToAdminLog = (writing: string) => {
+const WriteToAdminLog = (writing: string) => {
 
     fs.appendFileSync(path.resolve(__dirname, '../../../src', 'log', 'admin', 'admin_log.txt'), `---> ${writing}\n`);
 
 };
+
+export default WriteToAdminLog;
