0ab4f2d981e8240fe882995d4b3fa17977615873
mwang840 <maxwang@udel.edu>
2022-05-20 08:47:09 -0400

    changed package to include discord .js and updated linter/phrases files

---
+++ phrases_from_Diktovitch.ts
@@ -0,0 +1,9 @@
+interface Phrases {
+    phrase1: 'Give me rent!';
+    phrase2: 'Where is my money!?';
+    phrase3: 'This is a free country not a rent free country';
+    phrase4: 'Sorry doesn\'t pay the rent';
+    phrase5: 'Your months late, again!';
+}
+
+export default Phrases;
